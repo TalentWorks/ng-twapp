794980e837a3ebaf328fc089f0a82861fc2cb30c
Jose F. Sosa <josefsosa@gmail.com>
2014-10-10 11:43:22 -0700

    updated login mod

--- app/scripts/login-module/controllers/login-module-controller.js
+++ app/scripts/login-module/controllers/login-module-controller.js
@@ -5,10 +5,27 @@
  * @description # logingCtrl
  */
 
-angular.module('ng-twapp.login')
-    .controller('loginCtrl', function ($scope) {
-      // we will store all of our form data in this object
+angular.module('ng-twapp')
+    .controller('loginCtrl', function ($scope, loginService, $http, $parse) {
+
+      $http.get('http://localhost:9005/api/users').
+          success(function(data) {
+            var jsonData = angular.fromJson(data);
+            console.log(jsonData);
+            $scope.formData = jsonData;
+          });
+      console.log($scope.formData);
+
+      $scope.fname = $parse($scope.formData);
+
       $scope.formData = {};
+
+
+      $scope.users = loginService.getUsers;
+
+      ///$scope.usersByID = loginService.getUsersById('5433698078b5d6674be9680c');
+      //console.log($scope.usersByID);
+
 
       // function to process the form
       $scope.processForm = function () {
--- app/scripts/login-module/services/login-service.js
+++ app/scripts/login-module/services/login-service.js
@@ -5,7 +5,58 @@
  * @description # loginService
  */
 
-angular.module('ng-twapp.login')
-    .service('loginService', function ($rootScope) {
+angular.module('ng-twapp')
+    .service('loginService', function ($rootScope, $scope, $http) {
+      // Public API
+      var service = {};
+
+      var generateCommonAPIHeaders = function () {
+        return {
+          "Content-Type": "application/json"
+        };
+      };
+
+      service.getUsers = function () {
+        var deferred = $q.defer();
+        $http({
+          method: 'GET',
+          url:'localhost:9005/api/users',
+          headers: generateCommonAPIHeaders()
+        }).then(function (response) {
+              deferred.resolve(response);
+            });
+        return deferred.promise;
+      };
+
+      service.getUsersById = function (id) {
+        var deferred = $q.defer();
+        $http({
+          method: 'GET',
+          url:'localhost:9005/api/users' + id,
+          headers: generateCommonAPIHeaders()
+        }).then(function (response) {
+              deferred.resolve(response);
+            });
+        return deferred.promise;
+      };
+
+      service.createNewUser = function (userData) {
+        var deferred = $q.defer();
+        $http({
+          method: "POST",
+          data: userData,
+          url:'localhost:9005/api/users',
+          headers: generateCommonAPIHeaders()
+        })
+            .success(function (response) {
+              deferred.resolve(response._id);
+            })
+            .error(function (data, status) {
+              deferred.reject(status);
+            });
+        return deferred.promise;
+      }
+
+      return service;
 
     });
