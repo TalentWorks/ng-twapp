d1ee2167e42f676577fb4e183b8722b85bcc1f46
Jose F. Sosa <josefsosa@gmail.com>
2014-11-22 13:10:32 -0800

    Removed report files

--- generators/generator-ng-twapp-angular/generators/templates/grunt/usemin.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  html: ['<%= package.path.dist %>/{,*/}*.html'],
-  css: ['<%= package.path.tmp %>/styles/{,*/}*.css'],
-  options: {
-    assetsDirs: ['<%= package.path.dist %>','<%= package.path.dist %>/images']
-  }
-};
